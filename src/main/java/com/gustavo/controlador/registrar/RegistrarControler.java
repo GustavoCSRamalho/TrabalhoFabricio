3aa84d0f5d4d9fd41a6c54a042cb30492e052530
Gustavo <you@example.com>
2018-08-13 20:00:12 -0300

    Arrumado erro de importe

--- src/main/java/com/gustavo/controlador/registrar/RegistrarControler.java
+++ src/main/java/com/gustavo/controlador/registrar/RegistrarControler.java
@@ -1,7 +1,7 @@
 package com.gustavo.controlador.registrar;
 
+import com.gustavo.modelo.usuario.dto.UsuarioDTO;
 import com.gustavo.visao.usuario.UsuarioServico;
-import com.javapointers.controllers.modelo.usuario.dto.UsuarioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
